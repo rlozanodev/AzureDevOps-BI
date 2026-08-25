a5581b908083c4e255380d1b15e9c0562af82ceb
Robreto Lozano <lozanor.dev@gmail.com>
2026-08-25 15:12:48 +0000

    Clean Patch files from project

--- patch_vm2.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sed -i 's/private readonly ICatalogRepository _catalogRepository;/private readonly ICatalogRepository _catalogRepository;\n    private readonly ILogRepository _logRepository;/' src/AzureDevOps.DesktopManager/ViewModels/MainWindowViewModel.cs
